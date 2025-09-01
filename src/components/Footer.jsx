aa344a5ddc5b8d42b6009fdf1da60407b66d7a28
Alexandre Nkoua <alexnkoua4code@gmail.com>
2025-09-01 21:05:21 +0200

    mise en place du Header et du Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
+import styles from "./Footer.module.scss";
+
 function Footer() {
-	return <div className="b2">Footer</div>;
+	return (
+		<footer
+			className={`${styles.footer} d-flex flex-row align-item-center justify-content-center p-20`}
+		>
+			<p>Copyright © 2025 Cookchef Dyma, Inc</p>
+		</footer>
+	);
 }
 
 export default Footer;
